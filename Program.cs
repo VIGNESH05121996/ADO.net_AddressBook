aefcafcedfa0899a5f90507884f186a08bd36cfe
VIGNESH M <vickymarimuthu0512@gmail.com>
2021-12-03 19:42:27 +0530

    [VIGNESH M] Added: Welcome Message

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ADO.netAddressBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welccome To ADO.netAddressBook");
+        }
+    }
+}
