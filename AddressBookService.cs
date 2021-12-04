94ee0bd773966101ccd976773d8447bb1569c9b8
VIGNESH M <vickymarimuthu0512@gmail.com>
2021-12-04 20:03:29 +0530

    [VIGNESH M] Added: DeleteContact Method

--- AddressBookService.cs
+++ AddressBookService.cs
@@ -119,5 +119,32 @@ namespace ADO.netAddressBook
                 this.sqlConnection.Close();
             }
         }
+
+        public void DeleteContact()
+        {
+            try
+            {
+                this.sqlConnection.Open();
+                string query = @"delete from addresstable where FirstName='Praveen'";
+                SqlCommand sqlCommand = new SqlCommand(query, sqlConnection);
+                int result = sqlCommand.ExecuteNonQuery();
+                if (result != 0)
+                {
+                    Console.WriteLine("Deleted");
+                }
+                else
+                {
+                    Console.WriteLine("Not Deleted");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                this.sqlConnection.Close();
+            }
+        }
     }
 }
